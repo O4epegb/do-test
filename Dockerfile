df8a9ba601dab1acb0c67ec4e30bf5d406d2976d
Danila <fourlessthanthree@yahoo.com>
2017-09-15 01:03:12 +0300

    Add working docker compose example with postgres

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
-FROM node:8
+FROM node:alpine
 
 # Create app directory
 WORKDIR /usr/src/app
+
+RUN npm install pm2 -g --quiet
 
 # Install app dependencies
 # For npm@5 or later, copy package-lock.json as well
 COPY package.json package-lock.json ./
 
-RUN npm install pm2 -g
-RUN npm install
+RUN npm install --quiet
 
 # Bundle app source
 COPY . .
--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const showdown = require('showdown');
 
 const mdConverter = new showdown.Converter();
 const app = express();
 
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const pg = require('pg');
+
+const conString = process.env.DATABASE_URL;
+
+app.post('/users', function(req, res, next) {
+    const { user } = req.body;
+
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            // pass the error to the express error handler
+            return next(err);
+        }
+        client.query(
+            'INSERT INTO users (name, age) VALUES ($1, $2);',
+            [user.name, user.age],
+            function(err, result) {
+                done(); //this done callback signals the pg driver that the connection can be closed or returned to the connection pool
+
+                if (err) {
+                    // pass the error to the express error handler
+                    return next(err);
+                }
+
+                res.send(200);
+            }
+        );
+    });
+});
+
+app.get('/users', function(req, res, next) {
+    pg.connect(conString, function(err, client, done) {
+        if (err) {
+            // pass the error to the express error handler
+            return next(err);
+        }
+        client.query('SELECT name, age FROM users;', [], function(err, result) {
+            done();
+
+            if (err) {
+                // pass the error to the express error handler
+                return next(err);
+            }
+
+            res.json(result.rows);
+        });
+    });
+});
 
 app.get('/', function(req, res) {
     res.send(`
@@ -32,6 +84,11 @@ app.use((req, res, next) => {
     res.send('404');
 });
 
+app.use((err, req, res, next) => {
+    console.log(err, '500 error');
+    res.send('500 error');
+});
+
 app.listen(port, function() {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`App listening on port ${port}!`);
 });
